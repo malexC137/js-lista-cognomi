64756706a3e95a84f0567570de864c567e3ead6c
malexC137 <71665644+malexC137@users.noreply.github.com>
2021-04-14 09:32:39 +0200

    Added some comments

--- listaCognomi.js
+++ listaCognomi.js
@@ -1,10 +1,15 @@
+// dichiaro le variabili
 var surnamesList = ["46Rossi", "Iannone", "Petrucci", "Morbidelli", "Dovizioso"];
 var userSurname = prompt("Insesrisci il cognome del tuo pilota preferito");
+// creo un prompt per aggiungere un nuovo cognome all'array cognomi
 surnamesList.push(userSurname);
+// ordino la nuova array in ordine alfabetico
 surnamesList.sort();
+// creo una variabile che mi consenta di individuare il numero a cui corrisponde il nuove cognome inserito
 var indexUser = surnamesList.indexOf(userSurname) + 1;
 var numberedSurnamesList = document.getElementById("surnames-list");
 
+// creo un ciclo per stampare una lista di cognomi
 for (var i = 0; i < surnamesList.length; i++) {
     numberedSurnamesList.innerHTML += "<li><h2>" + (i + 1) + " - " + surnamesList.sort()[i] + "</h2></li>";
 }
